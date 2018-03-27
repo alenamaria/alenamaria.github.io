018ba6d1144bb7e017394b4d44e379de44b07c8b
alena <alena@Alena-Hurst.hfu-open.fu.hs-furtwangen.de>
2018-03-27 13:03:55 +0200

    Aufgabe 0 aus dem alenamaria.github.io Repository gelöscht

--- Aufgabe 1/js/Aufgabe1.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var name;
-    name = prompt("Hey, tell me what's your name!", "");
-    document.getElementById("name").innerHTML += "Nice to meet you, " + name + "!";
-});
--- Aufgabe 1/js/Aufgabe1.ts
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var name : string;
-
-    name = prompt("Hey, tell me what's your name!", "");
-    document.getElementById("name").innerHTML += "Nice to meet you, " + name + "!";
-})
