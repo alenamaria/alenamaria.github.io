8954c10e353bfb3e9f5bab4df060b433a7fee60b
alena <alena@192.168.2.40>
2018-03-30 20:10:49 +0200

    Aktualisierung der Userpage (Neue Links zu Aufgabe 1.2 und JavaScript-Einbindung)

---
+++ Userpage/js/userpage.ts
@@ -0,0 +1,44 @@
+// Generell - Scroll-Effekt: Konditionale Anweisung
+
+var h = window.innerHeight;
+
+let nav = document.getElementsByClassName('nav') as HTMLCollectionOf<HTMLElement>;
+
+window.addEventListener('scroll', function () {
+    if (nav.length != 0) {
+
+        // Festlegen, wann die Navigationsleiste 'fixed' ist und wann nicht
+        if (window.pageYOffset >= h) {
+            nav[0].style.position = 'fixed';
+        }
+
+        else {
+            nav[0].style.position = 'relative';
+        }
+    }
+
+});
+
+// Back-to-top Button
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    }
+
+    else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// Wenn der Button gedrueckt wird, springt es wieder zum Anfang der Seite
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
